d330c81ed80378676421d2cf2cc0fd2e3b9887ae
thiliphari31 <thiliphari31@gmail.com>
2025-07-01 15:22:51 +0530

    about page

---
+++ src/app/About/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">About Page</h1>
+      <p>This is the about section of my website.</p>
+    </main>
+  );
+}
